1823a9a446fc37025834934b13ed42c3c760d7be
ravi4all <ravikanttyagi.2295@gmail.com>
2019-02-09 16:37:42 +0530

    Add files via upload

---
+++ Regression/Logistic_Part-2.py
@@ -0,0 +1,33 @@
+import math
+import csv
+import random
+
+def load_dataset():
+    pass
+
+def str_to_float():
+    pass
+
+def minMax():
+    pass
+
+def normalization():
+    pass
+
+def prediction():
+    pass
+
+def accuracy_score():
+    pass
+
+def cross_validation():
+    pass
+
+def evaluate_algorithm():
+    pass
+
+def sgd_logistic():
+    pass
+
+def logisticRegression():
+    pass
